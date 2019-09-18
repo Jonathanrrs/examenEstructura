a33f02cafe08da2a4147ba7ddfb20da3b3c1d866
20156640 RUIZ SANDOVAL JONATHAN RAMIRO <20156640@TELEMATICA.UCOL.MX>
2019-09-18 15:42:56 -0500

    examen

---
+++ problema1/main.js
@@ -0,0 +1,35 @@
+let btn = document.querySelector("#btn").addEventListener("click", () => {
+    class numAmigos {
+        constructor() {
+
+        }
+
+        calcular() {
+            let suma1 = 0;
+            let suma2 = 0;
+            for (let i = 1; i <= 3000; i++) {
+                for (let x = 1; x <= 3000; x++) {
+                    if(i%x == 0) {
+                        suma1+=x;     
+                    }
+                    
+                }
+            }
+            for (let c = 1; c <= 3000; c++) {
+                for (let x2 = 1; x2 <= 3000; x2++) {
+                    if(c%x2 == 0) {
+                        suma2+=x2;
+                    }  
+                }
+            }
+            if(i == suma2 && c == suma1) {
+                console.log("son amigos");
+            }
+        }
+    }
+    let numeros = new numAmigos();
+    numeros.calcular();
+    
+
+});
+
